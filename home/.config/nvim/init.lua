f48a24c21fcbb5f0b17148647f3d7eb91f1776a8
The Game <chris.game@robbiebobbins.com>
2024-08-06 21:40:49 +0100

    use tab to navigate completion menu

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,4 +1,3 @@
-
 local g = vim.g       -- Global variables
 local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
 
@@ -128,6 +127,20 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end, {"i", "s"}),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
